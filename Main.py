f4d87455453eca534db36a99db8516aef8bf024c
2018grimonp <pierrick.grimon@student-cs.fr>
2018-11-19 15:47:56 +0100

    3eme commit

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-def initial(l):  # retire les '/n' d'un fichier texte 
+def initial(l):  # retire les '\n' d'un fichier texte 
     L=[]
     with open(l,'r') as fp:
         for line in fp:
@@ -12,4 +12,6 @@ print ('What is the file you want to analyse?')
 t=input()           #on rentre le fichier que l'on veut manipuler
 
 T=initial(t)
+
+
 
