977099d8944448eb981bc67feaaf163ff92eb93b
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-19 16:41:41 +0100

    V4 compte fonction avec affichage

--- trouve_fonction.py
+++ trouve_fonction.py
@@ -1,17 +1,17 @@
+import random as rd
 #liste de commande qui ouvre des actions
 open=["if","while","for","do","class"]
 
-"""
+
 #code à analyser
-Code = ["hello","def gilbert","end"]    #liste de ligne (str)
-"""
+Code1 = ["hello","def gilbert():","    lol","end","def bilibili(nhassah,aidj):","f","   if lol do","  end","end"]    #liste de ligne (str)
 
 ListeLongeurFonction=[] #donne une liste des n fonctions avec la longeur de chaque liste
            #indique le nombre de parentaises ouverte
 
-def ligne_open(k):      #controlle si la ligne ouvre des commande
+def ligne_open(k,Code):      #controlle si la ligne ouvre des commande
     for word in open:
-        if word in Code[k].slit(" "):
+        if word in Code[k].split(" "):
             return True
     return False
 
@@ -20,22 +20,46 @@ def count_fonction(Code):
     fonction_open=False
     def_open=-1
     for k in range(len(Code)):      #parcour le texte
-        if Code[:3]=="def" :
+        if Code[k][:3]=="def" :
             if fonction_open :                  #test si une fonction est déjà ouverte
                 return("Erreur fonction lingne "+str(k))
             else :
                 fonction_open=True
-            ListeLongeurFonction.append(k)      #sauvegarde l'indice de début de la fonction
+            ListeLongeurFonction.append({"longueur":k}) #sauvegarde l'indice de début de la fonction
+            ListeLongeurFonction[-1]["nom"]=Code[k][4:].split("(")[0]
             def_open=open_count
             open_count+=1
-        if ligne_open(k) :      #controle si la ligne ouvre des commandes
+        if ligne_open(k,Code) :      #controle si la ligne ouvre des commandes
             open_count+=1
-        if Code[:3]=="end" :    #controle si la ligne ferme des commandes
+        if "end" in Code[k].split(" ") :    #controle si la ligne ferme des commandes
             open_count-=1
             if open_count== def_open :
-                ListeLongeurFonction[-1]=k-ListeLongeurFonction[-1]-1   #calcul la longueur de la fonction (def et end exclus)
+                ListeLongeurFonction[-1]["longueur"]=k-ListeLongeurFonction[-1]["longueur"]-1   #calcul la longueur de la fonction (def et end exclus)
                 fonction_open=False
     return(ListeLongeurFonction)
 
 #omg c'est trop cool
+
+def affiche_fonction(Code):
+    fonctions=count_fonction(Code)
+    if len(fonctions)==0 :
+        print("omg ce candidat n'utilise pas de fonctions")
+        return(False)
+    l_moyenne,l_min,l_max=0,fonctions[0]["longueur"],fonctions[0]["longueur"]
+    for num_fonction in range(len(fonctions)):
+        print("fonction "+str(num_fonction))
+        print("nom : "+fonctions[num_fonction]["nom"])
+        print("longueur : "+str(fonctions[num_fonction]["longueur"])+"\n")
+        l_moyenne+=fonctions[num_fonction]["longueur"]
+        if fonctions[num_fonction]["longueur"]<l_min :
+            l_min=fonctions[num_fonction]["longueur"]
+        if fonctions[num_fonction]["longueur"]>l_max :
+            l_max=fonctions[num_fonction]["longueur"]
+    print ("la longueur moyenne est : "+str(l_moyenne/len(fonctions)))
+    print ("la longueur minimal est : "+str(l_min))
+    print ("la longueur maximal est : "+str(l_max))
+    print ("le nom de fonction le plus stylé est : "+fonctions[rd.randint(0,len(fonctions)-1)]["nom"])
+
+
+affiche_fonction(Code1)
 
