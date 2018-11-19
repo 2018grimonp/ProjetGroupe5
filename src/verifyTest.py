c88e00da911807ede104e1c36c2dcc9b8ad8d30c
Anthony <anthony.remy@student-cs.fr>
2018-11-19 11:56:39 +0100

    Commit du midi, le code est en plan :/

--- src/verifyTest.py
+++ src/verifyTest.py
@@ -10,6 +10,40 @@
 #
 # ==== ==== ==== ====
 
+# Liste des commande qui ouvrent des actions
+l_open=["if","while","for","do","class"]
+
+#degrossir + exception
+def rechercheEnd(lignes, ligneOpen):
+    """
+    Inspiré de count_fonction de trouve_fonction
+    :param lignes: Un tableau contenant des strings correspondantes aux différentes lignes du texte
+    :param ligneOpen: Le numéro de la ligne pour laquelle on recherche le end associé
+    :return: Le numéro de la ligne du end associé
+    """
+    # On suppose qu'un mot clé de open ne renvoie pas en end sur la même ligne si c'est le premier mot clé de open de sa ligne
+    open_count=0
+    fonction_open=False
+    def_open=-1
+    for k in range(len(lignes)):
+        if "end" in ligne
+        if Code[:3]=="def" :
+            if fonction_open :                  #test si une fonction est déjà ouverte
+                return("Erreur fonction lingne "+str(k))
+            else :
+                fonction_open=True
+            ListeLongeurFonction.append(k)      #sauvegarde l'indice de début de la fonction
+            def_open=open_count
+            open_count+=1
+        if ligne_open(k) :      #controle si la ligne ouvre des commandes
+            open_count+=1
+        if Code[:3]=="end" :    #controle si la ligne ferme des commandes
+            open_count-=1
+            if open_count== def_open :
+                ListeLongeurFonction[-1]=k-ListeLongeurFonction[-1]-1   #calcul la longueur de la fonction (def et end exclus)
+    return(ListeLongeurFonction)
+
+
 def isTest(lignes):
     """
     Vérifie que le fichier envoyé est bien un fichier de tests
@@ -28,6 +62,7 @@ def isTest(lignes):
     return False
 
 #v0.1
+# Ajouter isTest + exception
 def countTests(lignes):
     """
     Compte le nombre de tests dans un fichier et les séparent
