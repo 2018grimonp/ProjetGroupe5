6da2a29d733ba85b0e4deb6cba3668be229e4592
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-23 11:51:22 +0100

    geoffroy

--- affichageDuplication.py
+++ affichageDuplication.py
@@ -1,6 +1,19 @@
 import duplicationFonction
 import duplicationInterne
 
+def readLines(l):  # retire les '/n' d'un fichier texte
+    L=[]
+    fp=open(l,'r')
+    lines=fp.readlines()
+    for line in lines:
+        line=line.replace('\n', '')
+        L.append(line)
+    return L
+
+code=readLines("test.rb")
+codeControle=readLines("testControle.rb")
+precision=80
+
 def creation_string(code,codeControle,precision):
     donnees=duplicationFonction.retuour_resresultats_similitude(code,precision,codeControle)
     copies_percent=donnees[0]/donnees[1]
