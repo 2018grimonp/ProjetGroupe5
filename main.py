128c43554f22ece70b49ec6bdbdc7a22716ebb4d
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-23 10:40:50 +0100

    dd

--- main.py
+++ main.py
@@ -1,62 +1,29 @@
-from commentaires import retirerCom
-from commentaires import printCom 
-
-from checkIndentation import retirerIndentation
-from checkIndentation import printIndentation
-
-from trouve_variables import countVariables
-
-from check_case import returnmain 
-
-from trouve_fonction import printFonction
-
-from verifyTest import printStatsTests
-
-def readLines(l):  # retire les '/n' d'un fichier texte 
+def readLines(l):  # retire les '\n' d'un fichier texte 
     L=[]
-    fp=open(l,'r')
-    lines=fp.readlines()
-    for line in lines:
-        line=line.replace('\n', '')
-        L.append(line)
+    with open(l,'r') as fp:
+        for line in fp:
+            line=line.replace('\n', '')
+            L.append(line)
+    print(L)
     return L
 
-print ('Nom du fichier à analyser :')
+print ('What is the file you want to analyse?')
 
-#On rentre le fichier que l'on veut manipuler
-path=input()
-lines=readLines(path)
+t=input()           #on rentre le fichier que l'on veut manipuler
 
-#String qui représente le contenu du fichier results.txt qui sera fournis à l'interface graphique
-results_txt=""
+T=readLines(t)
 
-#Affichage d'informations sur les commentaires puis suppression des commentaires.
-print("----- COMMENTAIRES -----")
-results_txt=results_txt+printCom(lines)[0]
-lines=retirerCom(lines)
+def nbassert(lines):
+    """Compte le nombre de tests et d'asserts dans le fichier test"""
+    t=0                 #nombre de tests
+    a=0                 #nombre d'asserts
+    for i in lines :            #on regarde dans chaque élément de la liste combien il y a de test et d'assert 
+        if 'test' in i :        #il ne peut pas y avoir 'test' et 'assert' dans la meme ligne
+            t=t+1
+        elif 'assert' in i:
+            a=a+1
+    return (t,a)
 
-#Extraction et retrait des indentations et affichage d'informations sur la correction des indentations.
-print("----- INDENTATIONS -----")
-results_txt=results_txt+printIndentation(lines)
-lines=retirerIndentation(lines)
-
-#Parsing des fonctions présentes dans le code et affichage de leur nombre de lignes.
-print("----- FONCTIONS  -----")
-printFonction(lines)
-
-#Parsing des tests présents dans le code et affichage de données à leur sujet.
-print("----- VERIFICATION DES TESTS  -----")
-printStatsTests(lines)
-
-#Parsing des variables présentes dans le code et affichage d'informations à leur sujet.
-print("----- VARIABLES  -----")
-variables_list=countVariables(lines)
-results_txt=results_txt+returnmain(variables_list)[0]
-
-
-results=open("results.txt","w")
-results.write(results_txt)
-results.close()
 
 
 
