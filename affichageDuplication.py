86567d21b84a1676c6f41c31221195f020323454
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-23 13:47:25 +0100

    rectif

--- affichageDuplication.py
+++ affichageDuplication.py
@@ -15,7 +15,7 @@ precision=80
 
 def creation_string(code):
     donnees=duplicationFonction.retuour_resresultats_similitude(code,precision,codeControle)
-    copies_percent=donnees[0]/donnees[1]
+    copies_percent=donnees[0]/donnees[1]*100
     graph="Fonction copié-donne le nombre de fonctions suceptible d'e^tre fraudé (avec une précision minimale de "+str(precision)+"% -fonction originale+fonctions copiées-"+str(copies_percent)+"-Multiplicateur de note: "+str(copies_percent/10)+"/10 pourcentage de duplication :"+str(donnees[2])+"-|"
     return (graph)
 
