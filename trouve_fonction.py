79e2af0124640020ced5d87005f6a3e0a4680fb3
Jerkow <jerome.kowalski@student-cs.fr>
2018-11-23 14:18:02 +0100

    debug

--- trouve_fonction.py
+++ trouve_fonction.py
@@ -72,8 +72,10 @@ def printFonction(Code):
     print ("le nom de fonction le plus stylé est : "+fonctions[rd.randint(0,len(fonctions)-1)]["nom"])
     point = points(fonctions, l_moyenne/len(fonctions))
     analyse = c.analyseCom(Code)
-    graphique = 'Fonctions-'+point[1]+'-Lignes de fonctions+Lignes de commentaires+Reste des lignes-'+str(int(l_moyenne/len(Code)*100))+'+'+str(int(analyse[2]*10000)/100)+'-Note : '+str(point[0])+'/10-|'
+    graphique = 'Fonctions-'+point[1]+'-Lignes de fonctions+Lignes de commentaires+Reste des lignes-'+str(int(l_moyenne/len(Code)*100))+'+'+str(int(analyse[2]*10000/100))+'-Note : '+str(point[0])+'/10-|'
     return(graphique, point[0])
+
+
 
 def points(fonctions, l_moyenne):
     '''
@@ -123,4 +125,3 @@ def points(fonctions, l_moyenne):
                 note = 1
                 commentaire = 'Les fonctions sont trop grosses, il y en a beaucoup trop !'
     return note, commentaire
-
