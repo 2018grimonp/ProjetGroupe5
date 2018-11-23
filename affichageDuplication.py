027e2457c9f60a092c6d36b8c8bdc52aa584070c
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-23 14:43:30 +0100

    all

--- affichageDuplication.py
+++ affichageDuplication.py
@@ -19,5 +19,10 @@ def creation_string(code):
     graph="Fonctions copiées-Donne le nombre de fonctions suceptibles d'avoir été copiées (avec une précision minimale de "+str(precision)+"%)  pourcentage de duplication :"+str(donnees[2])+ "%-fonction originale+fonctions copiées-"+str(100-copies_percent)+"-Multiplicateur de note: "+str(1-copies_percent/100)+"-|"
     return (graph,1-copies_percent/100)
 
-def creation_string_duplication_interne():
-
+def creation_string_duplication_interne(Code):
+    liste_fonction=duplicationInterne.fonction_double(Code,precision)
+    l_double=0
+    for k in liste_fonction:
+        l_double+=k["longueur"]*k["copie"]
+    graph="Ligne de code inutile-Donne le pourcentage de lignes de code qui sont en double.-Lignes doublées+Lignes utiles -"+str(l_double/len(Code)*100)+"-Malus: -"+str(5*l_double/len(Code))+"-|"
+    return(graph,-5*l_double/len(Code))
